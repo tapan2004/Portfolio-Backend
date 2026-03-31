fe10cbfc3cc240c37da3e37d9cf869a7c89f0b79
Tapan Manna <130853662+tapan2004@users.noreply.github.com>
2026-03-31 10:16:47 +0530

    Update CorsConfig.java

--- src/main/java/com/springboot/api/config/CorsConfig.java
+++ src/main/java/com/springboot/api/config/CorsConfig.java
@@ -13,7 +13,7 @@ import java.util.List;
 @Configuration
 public class CorsConfig {
 
-    @Value("${app.cors.allowed-origins:http://localhost:5500,http://127.0.0.1:5500}")
+    @Value("${app.cors.allowed-origins:http://localhost:5173,http://127.0.0.1:5173}")
     private String[] allowedOrigins;
 
     @Bean
